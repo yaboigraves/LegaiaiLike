c123ebe4e6ea4ddfe0fdae6783f3d41c5db5e04f
Graves <gravesmakes.art@gmail.com>
2023-09-11 16:17:41 -0700

    billboard sprite swap prototype, 3d battle mockup with prototypes

--- scenes/prototypes/billboard/Billboard3D.gd
+++ scenes/prototypes/billboard/Billboard3D.gd
@@ -1,7 +1,44 @@
 
-extends AnimatedSprite3D
+extends Node3D
+
+@export var do_rotation = true
+@export var front : Texture2D
+@export var side : Texture2D
+@export var back : Texture2D
+
+#so
+#we need ONE shadow casting sprite 
+#then change the other stuff based on the forward direction
+#so we get the angle relative to the root really
 
 func _process(delta: float) -> void:
 	var cam = get_viewport().get_camera_3d()
 	
-	look_at(cam.global_position)
+	#get the dot product between the forward and the camera direction
+	
+	var camera_direction :Vector3= ( cam.global_position - position ).normalized()
+	
+	var dot = camera_direction.dot(transform.basis.z)
+	
+
+	if dot >= 0.5:
+		$Sprite.texture = front
+		
+		if !do_rotation:
+			$Sprite.rotation = Vector3(0,0,0)
+	elif dot > -0.5:
+		#depends what side we're on
+		#additinal data needed
+	
+		#$Sprite.flip_h = (position - cam.global_position).x > 0
+		$Sprite.texture = side
+		if !do_rotation:
+			$Sprite.rotation = Vector3(0,PI/2,0)
+	else:
+		$Sprite.texture = back
+		$Sprite.rotation = Vector3(0,PI,0)
+	#so dot is 1 when in front, -1 when in back
+
+	
+	if do_rotation:
+		$Sprite.look_at(cam.global_position)
